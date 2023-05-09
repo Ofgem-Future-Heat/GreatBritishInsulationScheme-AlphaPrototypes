850366be4a4de556eb90c36fb4afd916aa7d7413
Francisco Benedict Device Admin <BenedictF.admin@ofgem.gov.uk>
2023-05-09 16:09:13 +0100

    initial commit with site files

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,51 @@
+//
+// For guidance on how to add JavaScript see:
+// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
+//
+
+const pageUrl         =   window.location.pathname.split("/");
+const domainPathUrl   =   pageUrl[pageUrl.length-2];
+const currentUrl      =   pageUrl[pageUrl.length-1];
+const pageUrlPath     =   window.location.pathname;
+
+window.GOVUKPrototypeKit.documentReady(() => {
+  // Add JavaScript here
+    console.log('application.js is loaded');
+})
+
+function superUser() {
+  console.log('superUser here');
+  localStorage.setItem("userType", 'superUser');
+}
+
+function existingNormalUser() {
+  console.log('existingNormalUser here');
+  localStorage.setItem("userType", 'existingNormalUser');
+}
+
+function newNormalUser() {
+  console.log('newNormalUser here');
+  localStorage.setItem("userType", 'newNormalUser');
+}
+
+if (currentUrl === 'upload-a-measure') {
+  if (localStorage.getItem('userType') === 'superUser') {
+    console.log('superuser');
+    document.getElementById('startButton').setAttribute('href', 'user-management/sign-in');
+  } else {
+    console.log('existingNormalUser or newNormalUser here');
+    document.getElementById('startButton').setAttribute('href', 'user-management/sign-in-nu');
+  }
+}
+
+
+// // File upload validation
+const regexForCSV = new RegExp("(.*?)\.(csv)$");
+function triggerValidation(el) {
+  if (!(regexForCSV.test(el.value.toLowerCase()))) {
+    el.value = '';
+    window.location.pathname = "/v01/measures/invalid-file-upload";
+  } else {
+    window.location.pathname = "/v01/measures/success-file-upload";
+  }
+}
